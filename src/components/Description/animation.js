43a0861425d141cc19ec3e5c6dc92bc34c4cfe66
Linuk Perera <163678566+LinukPerera@users.noreply.github.com>
2025-06-24 21:58:45 +0530

    Update animation.js

--- src/components/Description/animation.js
+++ src/components/Description/animation.js
@@ -1,12 +1,16 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting){
-            entry.target.classList.add('show');
-        } else{
-            entry.target.classList.remove('show');
+export function runIntersectionAnimations() {
+  if (typeof window !== 'undefined' && 'IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        } else {
+          entry.target.classList.remove('show');
         }
+      });
     });
-});
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
+  }
+}
