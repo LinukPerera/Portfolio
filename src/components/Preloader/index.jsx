96700887f6d6681eaeb8a169cefe95738537d41f
Linuk Perera <163678566+LinukPerera@users.noreply.github.com>
2025-06-30 23:45:53 +0530

    Update index.jsx

--- src/components/Preloader/index.jsx
+++ src/components/Preloader/index.jsx
@@ -4,25 +4,25 @@ import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import { opacity, slideUp } from './anim';
 
-const words = ["Hello", "Bonjour", "Ciao", "Olà", "やあ", "Hallo", "Guten tag", "ආයුබෝවන්"]
+const words = ["Hello", "Bonjour", "Ciao", "Olà", "やあ", "Hallo", "Guten tag", "ආයුබෝවන්"];
 
 export default function Index() {
     const [index, setIndex] = useState(0);
     const [dimension, setDimension] = useState({width: 0, height:0});
 
-    useEffect( () => {
-        setDimension({width: window.innerWidth, height: window.innerHeight})
-    }, [])
+    useEffect(() => {
+        setDimension({width: window.innerWidth, height: window.innerHeight});
+    }, []);
 
-    useEffect( () => {
+    useEffect(() => {
         if(index == words.length - 1) return;
-        setTimeout( () => {
-            setIndex(index + 1)
-        }, index == 0 ? 1000 : 150)
-    }, [index])
+        setTimeout(() => {
+            setIndex(index + 1);
+        }, index == 0 ? 1000 : 150);
+    }, [index]);
 
-    const initialPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${dimension.height} Q${dimension.width/2} ${dimension.height + 300} 0 ${dimension.height}  L0 0`
-    const targetPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${dimension.height} Q${dimension.width/2} ${dimension.height} 0 ${dimension.height}  L0 0`
+    const initialPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${dimension.height} Q${dimension.width/2} ${dimension.height + 300} 0 ${dimension.height}  L0 0`;
+    const targetPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${dimension.height} Q${dimension.width/2} ${dimension.height} 0 ${dimension.height}  L0 0`;
 
     const curve = {
         initial: {
@@ -33,20 +33,11 @@ export default function Index() {
             d: targetPath,
             transition: {duration: 0.7, ease: [0.76, 0, 0.24, 1], delay: 0.3}
         }
-    }
+    };
 
     return (
-    return (
-        <motion.div 
-            variants={slideUp}
-            initial="initial"
-            exit="exit"
-            className={styles.introduction}
-            onAnimationComplete={() => {
-                // This ensures we don't unmount too early
-            }}
-        >
-        
+        <motion.div variants={slideUp} initial="initial" exit="exit" className={styles.introduction}>
+            {dimension.width > 0 && 
             <>
                 <motion.p variants={opacity} initial="initial" animate="enter"><span></span>{words[index]}</motion.p>
                 <svg>
@@ -55,5 +46,5 @@ export default function Index() {
             </>
             }
         </motion.div>
-    )
+    );
 }
