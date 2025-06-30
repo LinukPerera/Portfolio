5150a5b6f060759b1a572ec64b212e164d826be6
Linuk Perera <163678566+LinukPerera@users.noreply.github.com>
2025-06-30 23:33:33 +0530

    Update anim.js

--- src/components/Preloader/anim.js
+++ src/components/Preloader/anim.js
@@ -17,14 +17,3 @@ export const slideUp = {
         transition: { duration: 1.2, ease: [0.76, 0, 0.24, 1], delay: 0 } // Increased duration
     }
 }
-
-// Add this new animation for the preloader
-export const preloaderAnim = {
-    initial: {
-        opacity: 1
-    },
-    exit: {
-        opacity: 0,
-        transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 1 } // Matches with slideUp
-    }
-}
